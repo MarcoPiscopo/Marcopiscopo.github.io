123c24e9d25b748027e6a097ad3f103face0fa69
MarcoPiscopo <64895780+MarcoPiscopo@users.noreply.github.com>
2021-06-20 18:13:01 +0200

    WTF

---
+++ WTF/script.js
@@ -0,0 +1,179 @@
+//      ARRAY CON PEZZI DI VOLTI
+
+const volto = [
+    "url('Volti/V_a.png')", "url('Volti/V_b.png')", "url('Volti/V_c.png')", "url('Volti/V_d.png')",
+    "url('Volti/V_e.png')", "url('Volti/V_f.png')", "url('Volti/V_g.png')", "url('Volti/V_h.png')", 
+    "url('Volti/V_i.png')", "url('Volti/V_j.png')", "url('Volti/V_k.png')", "url('Volti/V_l.png')", 
+    "url('Volti/V_m.png')", "url('Volti/V_n.png')", "url('Volti/V_o.png')", "url('Volti/V_p.png')", 
+    "url('Volti/V_q.png')", "url('Volti/V_r.png')", "url('Volti/V_s.png')", "url('Volti/V_t.png')", 
+    "url('Volti/V_u.png')", "url('Volti/V_v.png')", "url('Volti/V_w.png')", "url('Volti/V_x.png')",
+    "url('Volti/V_y.png')", "url('Volti/V_z.png')", ];
+
+const naso = [
+    "url('Nasi/N_a.png')", "url('Nasi/N_b.png')", "url('Nasi/N_c.png')", "url('Nasi/N_d.png')",
+    "url('Nasi/N_e.png')", "url('Nasi/N_f.png')", "url('Nasi/N_g.png')", "url('Nasi/N_h.png')", 
+    "url('Nasi/N_i.png')", "url('Nasi/N_j.png')", "url('Nasi/N_k.png')", "url('Nasi/N_l.png')", 
+    "url('Nasi/N_m.png')", "url('Nasi/N_n.png')", "url('Nasi/N_o.png')", "url('Nasi/N_p.png')", 
+    "url('Nasi/N_q.png')", "url('Nasi/N_r.png')", "url('Nasi/N_s.png')", "url('Nasi/N_t.png')", 
+    "url('Nasi/N_u.png')", "url('Nasi/N_v.png')", "url('Nasi/N_w.png')", "url('Nasi/N_x.png')",
+    "url('Nasi/N_y.png')", "url('Nasi/N_z.png')", ];
+
+const capelli = [
+    "url('Capelli/C_a.png')", "url('Capelli/C_b.png')", "url('Capelli/C_c.png')", "url('Capelli/C_d.png')", 
+    "url('Capelli/C_e.png')", "url('Capelli/C_f.png')", "url('Capelli/C_g.png')", "url('Capelli/C_h.png')", 
+    "url('Capelli/C_i.png')", "url('Capelli/C_j.png')", "url('Capelli/C_k.png')", "url('Capelli/C_l.png')", 
+    "url('Capelli/C_m.png')", "url('Capelli/C_n.png')", "url('Capelli/C_o.png')", "url('Capelli/C_p.png')", 
+    "url('Capelli/C_q.png')", "url('Capelli/C_r.png')", "url('Capelli/C_s.png')", "url('Capelli/C_t.png')",
+    "url('Capelli/C_u.png')", "url('Capelli/C_v.png')", "url('Capelli/C_w.png')", "url('Capelli/C_x.png')", 
+    "url('Capelli/C_y.png')", "url('Capelli/C_z.png')", ];
+
+const occhi = [
+    "url('Occhi/O_a.png')", "url('Occhi/O_b.png')", "url('Occhi/O_c.png')", "url('Occhi/O_d.png')", 
+    "url('Occhi/O_e.png')", "url('Occhi/O_f.png')", "url('Occhi/O_g.png')", "url('Occhi/O_h.png')", 
+    "url('Occhi/O_i.png')", "url('Occhi/O_j.png')", "url('Occhi/O_k.png')", "url('Occhi/O_l.png')", 
+    "url('Occhi/O_m.png')", "url('Occhi/O_n.png')", "url('Occhi/O_o.png')", "url('Occhi/O_p.png')", 
+    "url('Occhi/O_q.png')", "url('Occhi/O_r.png')", "url('Occhi/O_s.png')", "url('Occhi/O_t.png')", 
+    "url('Occhi/O_u.png')", "url('Occhi/O_v.png')", "url('Occhi/O_w.png')", "url('Occhi/O_x.png')", 
+    "url('Occhi/O_y.png')", "url('Occhi/O_z.png')", ];
+
+const labbra = [
+    "url('Labbra/L_a.png')", "url('Labbra/L_b.png')", "url('Labbra/L_c.png')", "url('Labbra/L_d.png')",
+    "url('Labbra/L_e.png')", "url('Labbra/L_f.png')", "url('Labbra/L_g.png')", "url('Labbra/L_h.png')", 
+    "url('Labbra/L_i.png')", "url('Labbra/L_j.png')", "url('Labbra/L_k.png')", "url('Labbra/L_l.png')", 
+    "url('Labbra/L_m.png')", "url('Labbra/L_n.png')", "url('Labbra/L_o.png')", "url('Labbra/L_p.png')", 
+    "url('Labbra/L_q.png')", "url('Labbra/L_r.png')", "url('Labbra/L_s.png')", "url('Labbra/L_t.png')", 
+    "url('Labbra/L_u.png')", "url('Labbra/L_v.png')", "url('Labbra/L_w.png')", "url('Labbra/L_x.png')",
+    "url('Labbra/L_y.png')", "url('Labbra/L_z.png')", ];
+
+const corpo = [
+    "url('Corpi/C_a.png')", "url('Corpi/C_b.png')", "url('Corpi/C_c.png')", "url('Corpi/C_d.png')", 
+    "url('Corpi/C_e.png')", "url('Corpi/C_f.png')", "url('Corpi/C_g.png')", "url('Corpi/C_h.png')", 
+    "url('Corpi/C_i.png')", "url('Corpi/C_j.png')", "url('Corpi/C_k.png')", "url('Corpi/C_l.png')", 
+    "url('Corpi/C_m.png')", "url('Corpi/C_n.png')", "url('Corpi/C_o.png')", "url('Corpi/C_p.png')", 
+    "url('Corpi/C_q.png')", "url('Corpi/C_r.png')", "url('Corpi/C_s.png')", "url('Corpi/C_t.png')",
+    "url('Corpi/C_u.png')", "url('Corpi/C_v.png')", "url('Corpi/C_w.png')", "url('Corpi/C_x.png')", 
+    "url('Corpi/C_y.png')", "url('Corpi/C_z.png')", ];
+
+const barba = [
+    "url('Barba/P_a.png')", "url('Barba/P_b.png')", "url('Barba/P_c.png')", "url('Barba/P_d.png')",
+    "url('Barba/P_e.png')", "url('Barba/P_f.png')", "url('Barba/P_g.png')", "url('Barba/P_h.png')", 
+    "url('Barba/P_i.png')", "url('Barba/P_j.png')", "url('Barba/P_k.png')", "url('Barba/P_l.png')", 
+    "url('Barba/P_m.png')", "url('Barba/P_n.png')", "url('Barba/P_o.png')", "url('Barba/P_p.png')", 
+    "url('Barba/P_q.png')", "url('Barba/P_r.png')", "url('Barba/P_s.png')", "url('Barba/P_t.png')", 
+    "url('Barba/P_u.png')", "url('Barba/P_v.png')", "url('Barba/P_w.png')", "url('Barba/P_x.png')",
+    "url('Barba/P_y.png')", "url('Barba/P_z.png')", ]
+
+const altro = [
+    "url('Altro/A_a.png')", "url('Altro/A_b.png')", "url('Altro/A_c.png')", "url('Altro/A_d.png')",
+    "url('Altro/A_e.png')", "url('Altro/A_f.png')", "url('Altro/A_g.png')", "url('Altro/A_h.png')", 
+    "url('Altro/A_i.png')", "url('Altro/A_j.png')", "url('Altro/A_k.png')", "url('Altro/A_l.png')", 
+    "url('Altro/A_m.png')", "url('Altro/A_n.png')", "url('Altro/A_o.png')", "url('Altro/A_p.png')", 
+    "url('Altro/A_q.png')", "url('Altro/A_r.png')", "url('Altro/A_s.png')", "url('Altro/A_t.png')", 
+    "url('Altro/A_u.png')", "url('Altro/A_v.png')", "url('Altro/A_w.png')", "url('Altro/A_x.png')",
+    "url('Altro/A_y.png')", "url('Altro/A_z.png')", ];
+
+const sfondo = [
+    "url('Sfondo/S_a.jpg')", "url('Sfondo/S_b.png')", "url('Sfondo/S_c.png')", "url('Sfondo/S_d.png')",
+    "url('Sfondo/S_e.png')", "url('Sfondo/S_f.png')", "url('Sfondo/S_g.png')", "url('Sfondo/S_h.png')", 
+    "url('Sfondo/S_i.png')", "url('Sfondo/S_j.png')", "url('Sfondo/S_k.png')", "url('Sfondo/S_l.png')", 
+    "url('Sfondo/S_m.png')", "url('Sfondo/S_n.png')", "url('Sfondo/S_o.png')", "url('Sfondo/S_p.png')", 
+    "url('Sfondo/S_q.png')", "url('Sfondo/S_r.png')", "url('Sfondo/S_s.png')", "url('Sfondo/S_t.png')", 
+    "url('Sfondo/S_u.png')", "url('Sfondo/S_v.png')", "url('Sfondo/S_w.png')", "url('Sfondo/S_x.png')",
+    "url('Sfondo/S_y.png')", "url('Sfondo/S_z.png')", ];
+
+//      VARIABILE INCREMENTANTE
+
+var i = 0;
+
+//      FUNZIONE PRINCIPALE
+
+window.addEventListener('keydown', function(e){
+    myFunction = e.keyCode;
+    document.getElementById('inp').value = String.fromCharCode(myFunction);
+
+//     FUNZIONE CASUALE
+
+//     SWITCH CASE
+    if(myFunction == 8){
+
+        switch (i){
+            case 0:
+                document.getElementById('sfondo').style.backgroundImage = null;
+                break;
+            case 1:
+                document.getElementById('corpo').style.backgroundImage = null;
+                break;
+            case 2:
+                document.getElementById('volto').style.backgroundImage = null;
+                break;
+            case 3:
+                document.getElementById('barba').style.backgroundImage = null; 
+                break;
+            case 4:
+                document.getElementById('occhi').style.backgroundImage = null;
+                break;
+            case 5:
+                document.getElementById('labbra').style.backgroundImage = null;
+                break;
+            case 6:
+                document.getElementById('naso').style.backgroundImage = null;
+                break;
+            case 7:
+                document.getElementById('capelli').style.backgroundImage = null;
+                break;
+            case 8:
+                document.getElementById('altro').style.backgroundImage = null;
+                break;
+            default:
+                console.log(myFunction);
+                console.log(i + 'guarda');     
+        }
+        
+        i--
+
+        if(i<0){
+            i++
+        }
+
+    } else{
+
+    switch (i){
+        case 0:
+            document.getElementById('sfondo').style.backgroundImage = sfondo[myFunction - 65];
+            break;
+        case 1:
+            document.getElementById('corpo').style.backgroundImage = corpo[myFunction - 65];
+            break;
+        case 2:
+            document.getElementById('volto').style.backgroundImage = volto[myFunction - 65];
+            break;
+        case 3:
+            document.getElementById('barba').style.backgroundImage = barba[myFunction - 65]; 
+            break;
+        case 4:
+            document.getElementById('occhi').style.backgroundImage = occhi[myFunction - 65];
+            break;
+        case 5:
+            document.getElementById('labbra').style.backgroundImage = labbra[myFunction - 65];
+            break;
+        case 6:
+            document.getElementById('naso').style.backgroundImage = naso[myFunction - 65];
+            break;
+        case 7:
+            document.getElementById('capelli').style.backgroundImage = capelli[myFunction - 65];
+            break;
+        case 8:
+            document.getElementById('altro').style.backgroundImage = altro[myFunction - 65];
+            break;
+        default:
+            console.log(myFunction);    
+        }
+        if( myFunction == 32){ // annulla l'input della spacebar
+            i--
+    }
+
+i++
+console.log(i);
+
+    }
+})
